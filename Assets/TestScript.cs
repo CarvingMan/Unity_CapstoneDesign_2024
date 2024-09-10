b73fc3d23fecf62a12e18f880ee8614681169519
CarvingMan <130010242+CarvingMan@users.noreply.github.com>
2024-09-10 11:18:36 +0900

    test_commit2

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("그저 git 명령 테스트 위한 스크립트");
     }
 
     // Update is called once per frame
